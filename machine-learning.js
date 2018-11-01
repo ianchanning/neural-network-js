e0f4d50e425ae43212fe3af55d206c541af07a95
Ian Channing <ian@blank-slate.io>
2018-11-01 14:15:16 +0100

    Dev, code clean up and add linting

--- machine-learning.js
+++ machine-learning.js
@@ -1,34 +1,36 @@
-/**
- * This is an experiment to see if I can recreate funfunfunctions machine-learning / back-propagation code
- *
- * Step 1 is to recreate the JS code
- * Step 2 is to port to ReasonML
- */
+(() => {
+  "use strict"
+  /**
+   * This is an experiment to see if I can recreate funfunfunctions machine-learning / back-propagation code
+   *
+   * Step 1 is to recreate the JS code
+   * Step 2 is to port to ReasonML
+   */
+  const X_MAX = 400
+  const Y_MAX = 400
+  const rand = x => Math.random() * x
+  const points = Array(100).fill().map(() => ({
+    x: rand(X_MAX),
+    y: rand(Y_MAX)
+  }))
+  const draw = () => {
+    const el = (name) => document.createElementNS("http://www.w3.org/2000/svg", name)
+    const ci = point => {
+      let circle = el("circle")
+      circle.setAttribute("cx", point.x)
+      circle.setAttribute("cy", point.y)
+      circle.setAttribute("r",5)
+      // circle.setAttribute("style", "color:black")
+      return circle
+    }
 
-const rand = x => Math.random() * x
-const X_MAX = 400
-const Y_MAX = 400
-const points = Array(100).fill().map(x => ({
-  x: rand(X_MAX),
-  y: rand(Y_MAX)
-}))
+    let root = document.getElementById("root")
+    let svg = el("svg")
+    svg.setAttribute("height", Y_MAX)
+    svg.setAttribute("width", X_MAX)
 
-// console.log(points)
-
-let root = document.getElementById('root')
-let svg = document.createElementNS("http://www.w3.org/2000/svg",'svg')
-svg.setAttribute('height', Y_MAX)
-svg.setAttribute('width', X_MAX)
-svg.setAttribute('xmlns',"http://www.w3.org/2000/svg")
-const ci = point => {
-  let circle = document.createElementNS("http://www.w3.org/2000/svg",'circle')
-  circle.setAttribute('cx',point.x)
-  circle.setAttribute('cy',point.y)
-  circle.setAttribute('r',5)
-  // circle.setAttribute('style','color:black')
-  return circle
-}
-points.map(point => svg.appendChild(ci(point)))
-// svg.appendChild(ci(points[0]))
-document.body.appendChild(svg)
-
+    points.map(point => svg.appendChild(ci(point)))
+    root.appendChild(svg)
+  }
+  draw()
+})()
