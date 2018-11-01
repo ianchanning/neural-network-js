36b6646be7a7acd443651d2b31f585e6828e7f12
Ian Channing <ian@blank-slate.io>
2018-11-01 06:28:34 +0100

    Initial commit, get randomised SVG working

--- machine-learning.js
+++ machine-learning.js
@@ -1,1 +1,34 @@
-console.log('start me up')
+/**
+ * This is an experiment to see if I can recreate funfunfunctions machine-learning / back-propagation code
+ *
+ * Step 1 is to recreate the JS code
+ * Step 2 is to port to ReasonML
+ */
+
+const rand = x => Math.random() * x
+const X_MAX = 400
+const Y_MAX = 400
+const points = Array(100).fill().map(x => ({
+  x: rand(X_MAX),
+  y: rand(Y_MAX)
+}))
+
+// console.log(points)
+
+let root = document.getElementById('root')
+let svg = document.createElementNS("http://www.w3.org/2000/svg",'svg')
+svg.setAttribute('height', Y_MAX)
+svg.setAttribute('width', X_MAX)
+svg.setAttribute('xmlns',"http://www.w3.org/2000/svg")
+const ci = point => {
+  let circle = document.createElementNS("http://www.w3.org/2000/svg",'circle')
+  circle.setAttribute('cx',point.x)
+  circle.setAttribute('cy',point.y)
+  circle.setAttribute('r',5)
+  // circle.setAttribute('style','color:black')
+  return circle
+}
+points.map(point => svg.appendChild(ci(point)))
+// svg.appendChild(ci(points[0]))
+document.body.appendChild(svg)
+
